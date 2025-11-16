b83813ca50712b0c9f538b2abe0a7110e9ddfeeb
portakabin.in <portakabin.in@gmail.com>
2025-11-16 19:17:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -58,5 +58,5 @@ app.use((err, req, res, next) => {
 });
 
 // ---------- Start Server ----------
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
