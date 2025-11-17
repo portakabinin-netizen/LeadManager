6ea55478d3bd223c9d101ed4745d3b9b5789f91f
portakabin.in <portakabin.in@gmail.com>
2025-11-17 18:25:41 +0530

    Removed backend folder/file

--- backend/queries/leadsQueries.js
+++
@@ -1,9 +0,0 @@
-import api_url from "@/backend/routes/base_url";
-import axios from "axios";
-
-export const fetchLeadsByMobile = async (mobile, token) => {
-  const res = await axios.get(`${api_url}/leads/search/${mobile}`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-  return res.data;
-};
